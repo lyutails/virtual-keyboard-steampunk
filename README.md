67092f018fb15136b03efc25541615f9791edb0c
lyutails <lyusya.cool@gmail.com>
2022-05-05 19:54:48 +0300

    feat: start keyboard project preparing files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # virtual-keyboard-steampunk
+
+https://lyutails.github.io/virtual-keyboard-steampunk
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import './keyboard.scss'
+
+class Keyboard {
+    constructor(parent: HTMLElement)
+}
+
+
+
+/* class name
+text SVGTextContentElement
+appendFile
+
+append
+createattribute
+массив с буковками
+прогнать по массиву циклом
+onclick = function
+onmouse
+e.key - то что выдаёт буквами название кнопки
+
+класс это существительное
+он не может обрабатывать
+а его метод или функция внутри него может
+
+кнопка класс
+клавитура класс */
